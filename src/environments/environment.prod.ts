78f58790fdb6935a6d2871014f4b353676fae93b
Eugene Parhomenko <43147104+EugeneParhomenko@users.noreply.github.com>
2021-02-22 11:47:30 +0200

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import { Environment } from "src/app/shared/models/environment.model";
 
 export const environment: Environment = {
   production: true,
-  API_URL: "http://www.omdbapi.com/?apikey=d60e1936"
+  API_URL: "http://www.omdbapi.com/?apikey=d60e1936",
+  API_KEY: "d60e1936"
 };
